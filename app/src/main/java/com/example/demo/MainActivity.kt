2c109e0829fa1483ea9e362d7df2217c25691920
Cheesedz <chisnghia@gmail.com>
2024-03-11 21:51:35 +0700

    update

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
+import com.example.demo.AirPlaneModeChange
 
 class MainActivity : ComponentActivity() {
+    //Intent demo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,9 +18,6 @@ class MainActivity : ComponentActivity() {
         sendButton.setOnClickListener {
             var intent = Intent(Intent.ACTION_SEND) // intent
             intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_EMAIL, "niranjantest@gmail.com")
-            intent.putExtra(Intent.EXTRA_SUBJECT, "This is a dummy message")
-            intent.putExtra(Intent.EXTRA_TEXT, "Dummy test message")
             startActivity(intent)
         }
 
@@ -28,4 +28,33 @@ class MainActivity : ComponentActivity() {
             startActivity(intent)
         }
     }
+
+    /*--------------------------------------------------------------------*/
+    //
+    // register the receiver in the main activity in order to receive updates of broadcasts events if they occur
+//    lateinit var receiver: AirPlaneModeChange
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        receiver = AirPlaneModeChange()
+//
+//        // Intent Filter is useful to determine which apps wants to receive
+//        // which intents,since here we want to respond to change of
+//        // airplane mode
+//        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+//            // registering the receiver
+//            // it parameter which is passed in  registerReceiver() function
+//            // is the intent filter that we have just created
+//            registerReceiver(receiver, it)
+//        }
+//    }
+//
+//    // since AirplaneModeChangeReceiver class holds a instance of Context
+//    // and that context is actually the activity context in which
+//    // the receiver has been created
+//    override fun onStop() {
+//        super.onStop()
+//        unregisterReceiver(receiver)
+//    }
 }
