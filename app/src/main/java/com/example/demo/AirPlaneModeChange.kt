89e78455012d5a3076abf0d73f60d3873fdd92fb
Cheesedz <chisnghia@gmail.com>
2024-03-12 15:56:03 +0700

    update

--- app/src/main/java/com/example/demo/AirPlaneModeChange.kt
+++ app/src/main/java/com/example/demo/AirPlaneModeChange.kt
@@ -3,15 +3,23 @@ package com.example.demo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.TextView
 import android.widget.Toast
 
 // AirplaneModeChangeReceiver class extending BroadcastReceiver class
 class AirPlaneModeChange : BroadcastReceiver() {
+    var textView: TextView? = null
+
+    // Function to set the TextView instance
+    fun setTextViewForAirplaneMode(tv: TextView?) {
+        textView = tv
+    }
 
     // this function will be executed when the user changes his airplane mode
     override fun onReceive(context: Context?, intent: Intent?) {
 
         // intent contains the information about the broadcast in our case broadcast is change of airplane mode
+        // TextView instance to update its content
 
         // if getBooleanExtra contains null value,it will directly return back
         val isAirplaneModeEnabled = intent?.getBooleanExtra("state", false) ?: return
@@ -19,10 +27,12 @@ class AirPlaneModeChange : BroadcastReceiver() {
         // checking whether airplane mode is enabled or not
         if (isAirplaneModeEnabled) {
             // showing the toast message if airplane mode is enabled
-            Toast.makeText(context, "Airplane Mode Enabled", Toast.LENGTH_LONG).show()
+            //Toast.makeText(context, "Airplane Mode Enabled", Toast.LENGTH_LONG).show()
+            textView?.text = "Airplane Mode Enabled"
         } else {
             // showing the toast message if airplane mode is disabled
-            Toast.makeText(context, "Airplane Mode Disabled", Toast.LENGTH_LONG).show()
+            //Toast.makeText(context, "Airplane Mode Disabled", Toast.LENGTH_LONG).show()
+            textView?.text = "Airplane Mode Disabled"
         }
     }
 }
--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -4,57 +4,63 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import com.example.demo.AirPlaneModeChange
 
 class MainActivity : ComponentActivity() {
     //Intent demo
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // send button on click listener
-        val sendButton = findViewById<Button>(R.id.sendButton)
-        sendButton.setOnClickListener {
-            var intent = Intent(Intent.ACTION_SEND) // intent
-            intent.type = "text/plain"
-            startActivity(intent)
-        }
-
-        // View on click listener
-        val buttonView = findViewById<Button>(R.id.buttonView)
-        buttonView.setOnClickListener {
-            var intent = Intent(Intent.ACTION_VIEW)
-            startActivity(intent)
-        }
-    }
-
-    /*--------------------------------------------------------------------*/
-    //
-    // register the receiver in the main activity in order to receive updates of broadcasts events if they occur
-//    lateinit var receiver: AirPlaneModeChange
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
 //
-//        receiver = AirPlaneModeChange()
+//        // send button on click listener
+//        val sendButton = findViewById<Button>(R.id.sendButton)
+//        sendButton.setOnClickListener {
+//            var intent = Intent(Intent.ACTION_SEND) // intent
+//            intent.type = "text/plain"
+//            startActivity(intent)
+//        }
 //
-//        // Intent Filter is useful to determine which apps wants to receive
-//        // which intents,since here we want to respond to change of
-//        // airplane mode
-//        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
-//            // registering the receiver
-//            // it parameter which is passed in  registerReceiver() function
-//            // is the intent filter that we have just created
-//            registerReceiver(receiver, it)
+//        // View on click listener
+//        val buttonView = findViewById<Button>(R.id.buttonView)
+//        buttonView.setOnClickListener {
+//            var intent = Intent(Intent.ACTION_VIEW)
+//            startActivity(intent)
 //        }
 //    }
-//
-//    // since AirplaneModeChangeReceiver class holds a instance of Context
-//    // and that context is actually the activity context in which
-//    // the receiver has been created
-//    override fun onStop() {
-//        super.onStop()
-//        unregisterReceiver(receiver)
-//    }
+
+    /*--------------------------------------------------------------------*/
+    //
+    // register the receiver in the main activity in order to receive updates of broadcasts events if they occur
+    lateinit var receiver: AirPlaneModeChange
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonView = findViewById<Button>(R.id.buttonView)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        // Set up the AirPlaneModeChangeReceiver
+        receiver = AirPlaneModeChange()
+        receiver.setTextViewForAirplaneMode(textView)
+
+        // Intent Filter is useful to determine which apps wants to receive
+        // which intents,since here we want to respond to change of
+        // airplane mode
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            // registering the receiver
+            // it parameter which is passed in  registerReceiver() function
+            // is the intent filter that we have just created
+            registerReceiver(receiver, it)
+        }
+    }
+
+    // since AirplaneModeChangeReceiver class holds a instance of Context
+    // and that context is actually the activity context in which
+    // the receiver has been created
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
 }
